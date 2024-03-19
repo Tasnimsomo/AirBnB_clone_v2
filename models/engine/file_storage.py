cfb6e5fac96aa0abd568e27c9ab1e7a8bb857bf4
root <root@TasnimSomo>
2024-03-19 09:51:57 +0400

    0x02. AirBnB clone - MySQL

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,13 @@
 Update FileStorage: (models/engine/file_storage.py)
 """
 import json
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class FileStorage:
@@ -35,14 +42,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
         classes = {
             'BaseModel': BaseModel, 'User': User, 'Place': Place,
             'State': State, 'City': City, 'Amenity': Amenity,
