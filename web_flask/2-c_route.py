59fd56dd608903be408c286efa86654d31f588d9
root <root@TasnimSomo>
2024-04-19 08:23:44 +0400

    0x04. AirBnB clone - Web framework

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application listening on 0.0.0.0, port 5000.
+It defines a route that displays "Hello HBNB!" when accessed.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Route function for the root URL."""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def start():
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text():
+    return "c {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application listening on 0.0.0.0, port 5000.
+It defines a route that displays "Hello HBNB!" when accessed.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Route function for the root URL."""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def start():
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    text = text.replace('_', ' ')
+    return "c {}".format(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
