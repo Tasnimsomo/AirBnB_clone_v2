22aea7f0e42fe2f569bfff51834bd763958b690e
root <root@TasnimSomo>
2024-04-19 12:10:06 +0400

    0x04. AirBnB clone - Web framework

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application:
+""" Write a script that starts a Flask web application:
 Your web application must be listening on 0.0.0.0, port 5000
-must use the option strict_slashes=False in your route definition
 """
 
 from flask import Flask
 
 app = Flask("__name__")
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return 'Hello HBNB!'
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    return 'HBNB'
+    """Returns a given string"""
+    return ("HBNB")
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    # Replace underscores with spaces
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
