e0152b349bcca4d6068a9e9b2386b0cc7a10a89b
root <root@TasnimSomo>
2024-04-19 12:08:09 +0400

    0x04. AirBnB clone - Web framework

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,28 +4,24 @@ starts a Flask web application:
 Your web application must be listening on 0.0.0.0, port 5000
 must use the option strict_slashes=False in your route definition
 """
+
 from flask import Flask
 
-
-app = Flask(__name__)
-
+app = Flask("__name__")
 
 @app.route('/', strict_slashes=False)
-def route():
-    """display Hello HBNB"""
-    return ("Hello HBNB!")
-
+def hello_hbnb():
+    return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
-def route2():
-    """display HBNB"""
-    return ("HBNB")
-
+def hbnb():
+    return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    return "C " + text.replace("_", " ")
+def c_text(text):
+    # Replace underscores with spaces
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
