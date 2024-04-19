750566e8e934b5c0fb606397ef8fae9ec22d4ad4
root <root@TasnimSomo>
2024-04-19 12:28:06 +0400

    0x04. AirBnB clone - Web framework

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,39 +1,47 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application:
-Your web application must be listening on 0.0.0.0, port 5000
-must use the option strict_slashes=False in your route definition
-"""
-from flask import Flask, render_templaite
-
+"""Flask"""
+from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return 'Hello HBNB!'
+def hello():
+    """Return a given string"""
+    return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB'
+    """Returns a given string"""
+    return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    text = text.replace('_', ' ')
-    return f'C {text}'
+def cText(text):
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    text = text.replace('_', ' ')
-    return f'Python {text}'
+def pythonText(text="is cool"):
+    """display Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    return f'{n} is a number'
+def isNumber(n):
+    """display “n is a number” only if n is an integer"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('number_template.html', number=n)
+def number_template(n=None):
+    """display a HTML page only if n is an integer"""
+    if isinstance(n, int):
+        return render_template("5-number.html", n=n)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run()
